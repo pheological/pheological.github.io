8e0ff996131074242644dfeb2d2fdcbdece31ead
Pheological <isprofortnite@gmail.com>
2025-11-24 11:58:27 -0800

    aaaaa

--- vite.config.ts
+++
@@ -1,9 +0,0 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  // IMPORTANT: This must match your repository name with slashes
-  base: '/website/', 
-})
